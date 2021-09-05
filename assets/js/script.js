474a421257499dc5a30d6ed839d9762f7221a5eb
Sivan Kaplan <sivanagar@gmail.com>
2021-09-04 19:42:57 -0700

    saving and loadin local storage search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
+var searchHistory = []
+var searchCityInput = ""
 
+
+var displayHistory = function() {
+    searchHistory = JSON.parse(localStorage.getItem("citySearched"));
+    if (!searchHistory) {
+        searchHistory = [];
+        return
+    }
+    $(searchHistory).each(function(index) {
+        addHistory(index)
+    })
+}
+
+var addHistory = function (index) {
+    var searchItem = $("<li>")
+            .addClass("searchItem btn btn-outline-secondary")
+            .attr("data-id",index)
+            .text(searchHistory[index])
+    $("#searchHistory").append(searchItem);
+}
 
 function searchCity() {
- console.log("search")
+    //send to api
+    //update search history
+    console.log("API")
 }
+
+$("#search").on("click",function(event) {
+    event.preventDefault();   
+    searchCityInput = $("#searchCity")
+        .val()
+        .trim();
+    searchCity();
+    searchHistory.push(searchCityInput);
+    addHistory(searchHistory.length - 1);
+    //save search localStorage
+    localStorage.setItem("citySearched", JSON.stringify(searchHistory));
+    
+});
+
+displayHistory();
