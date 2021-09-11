38c503ae23d28142c839fdc6839cc4289ab63747
Sivan Kaplan <sivanagar@gmail.com>
2021-09-11 09:53:38 -0700

    google city autocomplete api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var searchHistory = []
-var searchCityInput = ""
+var searchCityInput = document.getElementById("searchCity");
 var apiKey="ef360dfd13065444f31dda06f4972fc0"
+var todayWeather = {}
 
 
 var displayHistory = function() {
@@ -16,7 +17,7 @@ var displayHistory = function() {
 
 var addHistory = function (index) {
     var searchItem = $("<li>")
-            .addClass("list-group-item list-group-item-action")
+            .addClass("list-group-item list-group-item-action searchItem")
             .attr("data-id",index)
             .text(searchHistory[index])
     $("#searchHistory").append(searchItem);
@@ -31,8 +32,10 @@ function searchCity() {
         .then(function(data) {
             //use data to show info
             console.log(data);
-            var todayWeather = {
+            todayWeather = {
                 "city" : data.name,
+                "coord" : {"lat": data.coord.lat,
+                            "lon": data.coord.lon},
                 "date" : data.dt,
                 "icon": data.weather[0].icon,
                 "temperature" : data.main.temp,
@@ -41,18 +44,22 @@ function searchCity() {
                 "uvIndex" : ""
             }
             console.log(todayWeather)
+            fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+ todayWeather.coord.lat+'&lon='+ todayWeather.coord.lon+'&units=imperial&appid='+apiKey)
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data)
+                })
+            
         })
     
     
 }
 
 
-
 $("#search").on("click",function(event) {
     event.preventDefault();   
-    searchCityInput = $("#searchCity")
-        .val()
-        .trim();
     searchCity();
     searchHistory.push(searchCityInput);
     addHistory(searchHistory.length - 1);
@@ -62,3 +69,30 @@ $("#search").on("click",function(event) {
 });
 
 displayHistory();
+//$(".searchItem").on("click",function(event) {
+//    searchCityInput = $(this)
+//        .text();
+//    searchCity();
+//})
+let autocomplete;
+
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(searchCityInput, {
+    types: ["(cities)"],
+    componentRestrictions: {
+      country: "us",
+    },
+  });
+  autocomplete.addListener("place_changed", onCityChanged);
+}
+
+function onCityChanged() {
+    var place = autocomplete.getPlace();
+    //   console.log("place from Google API", place.address_components[2].short_name);
+    cityLat = place.geometry.location.lat();
+    cityLng = place.geometry.location.lng();
+    cityName = place.vicinity;
+    cityState = place.address_components[2].short_name;
+    
+}
+  
